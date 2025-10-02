ddbb4ec1c9928e9906a93c0df1b8dcdeb7fc3cb3
Affuyfuffyt <98175855+Affuyfuffyt@users.noreply.github.com>
2025-10-02 20:59:34 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-    // Note: `output: 'export'` is not compatible with Firebase integration.
-    // We are removing it to enable server-side functionalities needed for Firebase.
+    output: 'export',
 };
 
 export default nextConfig;
